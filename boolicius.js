844c384d67358d13eed015522d2af0193c4849fa
Hannes Landeholm <hannes@jumpstarter.io>
2014-02-24 01:45:11 +0100

    first commit

---
+++ boolicius.js
@@ -0,0 +1,332 @@
+var uid_seq =  0;
+var conjunctions = [];
+
+var Bit = function() {
+    // Assign unique id to this bit state.
+    this.id = "x" + uid_seq;
+    uid_seq += 1;
+    // A new bit is undefined.
+    this.defined = false;
+};
+
+Bit.prototype.toString = function() {
+    return this.id;
+};
+
+global.assert = function assert(v) {
+    if (!v)
+        throw new Error("assertion failed");
+};
+
+var build_formula = function(raw_formula, map) {
+    for (var key in map) {
+        var bit = map[key];
+        assert(bit instanceof Bit && bit.defined);
+        raw_formula = raw_formula.replace(new RegExp(key, 'g'), bit.toString());
+    }
+    return raw_formula;
+};
+
+/// Constrains an existing bit.
+global.constrain_bit = function constrain_bit(bit, is_set) {
+    assert(bit instanceof Bit && bit.defined);
+    conjunctions.push("(" + (is_set? "": "!") + bit + ")");
+};
+
+/// Defines a new bit.
+global.define_bit = function define_bit(is_set) {
+    var bit = new Bit();
+    conjunctions.push("(" + (is_set? "": "!") + bit + ")");
+    bit.value = !!is_set;
+    bit.defined = true;
+    return bit;
+};
+
+/// Defines a new input bit.
+global.define_input_bit = function define_input_bit() {
+    var bit = new Bit();
+    bit.defined = true;
+    return bit;
+};
+
+/// Defines a new bit based on the specified raw formula and adds a conjunction for it.
+var define_bit_formula = function(raw_formula, map) {
+    var bit = new Bit();
+    conjunctions.push("((" + build_formula(raw_formula, map) + ") <-> " + bit + ")");
+    bit.defined = true;
+    return bit;
+};
+
+/// Defines a new bit that should be the inverse result of another bit.
+global.not = function not(x) {
+    return define_bit_formula("!x", {x: x});
+};
+
+/// Defines a new bit that should be the anded result of two other bits.
+global.and = function and(a, b) {
+    return define_bit_formula("(a & b)", {a: a, b: b});
+};
+
+/// Defines a new bit that should be the ored result of two other bits.
+global.or = function or(a, b) {
+    return define_bit_formula("(a | b)", {a: a, b: b});
+};
+
+/// Defines a new bit that should be the xored result of two other bits.
+global.xor = function xor(a, b) {
+    return define_bit_formula("(a | b) & !(a & b)", {a: a, b: b});
+};
+
+/// Defines a new bit vector where each bit is a function of the bits with
+/// the same index in two input vectors.
+var fn_bv = function(a_bv, b_bv, bit_fn) {
+    assert(a_bv.length === b_bv.length);
+    var o_bv = new Array(a_bv.length);
+    for (var i = 0; i < a_bv.length; i++)
+        o_bv[i] = bit_fn(a_bv[i], b_bv[i]);
+    return o_bv;
+};
+
+/// Defines a new bit vector that is the inverse result of another bit vector.
+global.not_bv = function not_bv(x_bv) {
+    var o_bv = new Array(x_bv.length);
+    for (var i = 0; i < x_bv.length; i++)
+        o_bv[i] = not(x_bv[i]);
+    return o_bv;
+};
+
+/// Defines a new bit vector that is the anded result of two other bit vectors.
+global.and_bv = function and_bv(a_bv, b_bv) {
+    return fn_bv(a_bv, b_bv, and);
+};
+
+/// Defines a new bit vector that is the ored result of two other bit vectors.
+global.or_bv = function or_bv(a_bv, b_bv) {
+    return fn_bv(a_bv, b_bv, or);
+};
+
+/// Defines a new bit vector that is the xored result of two other bit vectors.
+global.xor_bv = function xor_bv(a_bv, b_bv) {
+    return fn_bv(a_bv, b_bv, xor);
+};
+
+/// Defines a new bit vector which is the result of the unsigned addition of
+/// two input bitvectors of equal size. Index 0 is considered the least
+/// significant bit and the significance is a linear function of the index.
+/// If modulus is true the final carry bit is not generated and will be
+/// returned as null. Returns an array containing [out_bitvector, final_carry].
+global.uadd_bv = function uadd_bv(a_bv, b_bv, modulus) {
+    assert(a_bv.length === b_bv.length);
+    var o_bv = new Array(a_bv.length);
+    var prev_carry;
+    for (var i = 0; i < a_bv.length; i++) {
+        var a = a_bv[i];
+        var b = b_bv[i];
+        var out, carry;
+        if (i === 0) {
+            // Half adder for least signficant bit.
+            out = xor(a, b);
+            carry = define_bit_formula("a & b", {a: a, b: b});
+        } else {
+            // Full adder for other bits.
+            out = xor(xor(a, b), prev_carry);
+            if (modulus && i === a_bv.length - 1) {
+                // Throwing away the last carry bit for modulus addition.
+                carry = null;
+            } else {
+                carry = define_bit_formula("(a & b) | (pc & a) | (pc & b)", {a: a, b: b, pc: prev_carry});
+            }
+        }
+        o_bv[i] = out;
+        prev_carry = carry;
+    }
+    return [o_bv, carry];
+};
+
+/// Shorthand for uadd_bv with modulus = true and adding an arbitrary number
+/// of bit vectors together. Returns only the final out_bitvector.
+global.uaddm_bv = function uaddm_bv(a_bv, b_bv) {
+    var args = Array.prototype.slice.call(arguments, 0);
+    assert(args.length >= 2);
+    do {
+        var a_bv = args.pop();
+        var b_bv = args.pop();
+        args.push(uadd_bv(a_bv, b_bv, true)[0]);
+    } while (args.length > 1);
+    return args.pop();
+};
+
+/// Defines a new bit vector that is the input vector right rotated the specified number of steps.
+global.rotr_bv = function rotr_bv(bv, n) {
+    assert(n > 0 && n < bv.length);
+    var o_bv = new Array(bv.length);
+    for (var i = 0; i < bv.length; i++)
+        o_bv[i] = bv[(i + n) % bv.length];
+    return o_bv;
+};
+
+/// Defines a new bit vector that is the input vector right shifted the specified number of steps.
+global.shr_bv = function shr_bv(bv, n) {
+    assert(n > 0 && n < bv.length);
+    var o_bv = new Array(bv.length);
+    for (var i = 0; i < bv.length - n; i++)
+        o_bv[i] = bv[i + n];
+    for (var i = bv.length - n; i < bv.length; i++)
+        o_bv[i] = define_bit(false);
+    return o_bv;
+};
+
+/// Defines an input bit vector with specified name and length.
+global.define_input_bv = function define_input_bv(n) {
+    assert(n > 0);
+    var o_bv = new Array(n);
+    for (var i = 0; i < n; i++)
+        o_bv[i] = define_input_bit();
+    return o_bv;
+};
+
+/// Defines a new bit vector initialized with the specified value.
+global.define_bv = function define_bv(n, init) {
+    assert(n > 0 & n <= 32);
+    var o_bv = new Array(n);
+    for (var i = 0; i < n; i++) {
+        o_bv[i] = define_bit((init & 1) !== 0);
+        init = (init >> 1);
+    }
+    return o_bv;
+};
+
+/// Defines a new bit vector based on the specified hexadecimal input string.
+global.define_bv_hex = function define_bv_hex(hex_str) {
+    assert(hex_str.length > 0);
+    var o_bv = new Array(hex_str.length * 4);
+    for (var i = 0; i < hex_str.length; i++) {
+        var bit4 = parseInt(hex_str.charAt(i), 16);
+        for (var j = 0; j < 4; j++) {
+            o_bv[i * 4 + 3 - j] = define_bit((bit4 & 1) !== 0);
+            bit4 = (bit4 >> 1);
+        }
+    }
+    return o_bv;
+};
+
+/// Takes an already defined bit vector and adds additional constraints.
+/// Only the bits that are set in the mask are constrained.
+global.constrain_bv = function constrain_bv(bv, set, mask) {
+    assert(bv.length > 0 & bv.length <= 32);
+    for (var i = 0; i < bv.length; i++) {
+        if ((mask & 1) !== 0) {
+            console.log("constraining bit [" + i + "] to [" + ((set & 1) !== 0) + "]");
+            constrain_bit(bv[i], (set & 1) !== 0);
+        }
+        set = (set >> 1);
+        mask = (mask >> 1);
+    }
+};
+
+var z2 = function(n) {
+    n = n.toString();
+    return (n.length < 2)? "0" + n: "" + n;
+};
+
+var get_time_str = function() {
+    var now = new Date();
+    var yyyy = now.getUTCFullYear() + "";
+    var mm = z2(now.getUTCMonth() + 1);
+    var dd = z2(now.getUTCDate());
+    var hours = z2(now.getUTCHours());
+    var minutes = z2(now.getUTCMinutes());
+    var seconds = z2(now.getUTCSeconds());
+    return yyyy + mm + dd + hours + minutes + seconds;
+};
+
+/// Attempts to satisfy all logical conjunctions defined in the problem.
+/// Takes an object of "main bit vectors" that are of particular intrest
+/// that will be presented in a human readable format. "main_bv" should
+/// be an object of human readable keys mapped to bit vectors.
+global.satisfy = function satisfy(main_bv) {
+    // Define locations where problem stages are stored.
+    var uid = get_time_str();
+    var pdir = "problem-" + uid;
+    var pin_path = pdir + "/problem.in";
+    var pcnf_path = pdir + "/problem.cnf";
+    var pout_path = pdir + "/problem.out";
+    var psum_path = pdir + "/problem.sum";
+    // Create main problem directory for this job.
+    var fs = require('fs');
+    fs.mkdirSync(pdir);
+    // Write input problem file.
+    console.log("writing problem [" + pin_path + "]");
+    fs.writeFileSync(pin_path, conjunctions.join("  &  "));
+    // Convert input problem to cnf.
+    console.log("converting problem to cnf [" + pcnf_path + "]");
+    var cp = require('child_process');
+    var limboole = cp.spawn("limboole", ["-d", "-s", pin_path, "-o", pcnf_path], {stdio: ["ignore", "ignore", process.stderr]});
+    limboole.on("exit", function(code, signal) {
+        if (code !== 0)
+            throw new Error("limboole exited with [" + code + "]");
+        // Solve problem.
+        console.log("solving problem [" + pout_path + "]");
+        var minisat = cp.spawn("minisat", [pcnf_path, pout_path], {stdio: ["ignore", process.stdout, process.stderr]});
+        minisat.on("exit", on_minisat_exit);
+    });
+    var on_minisat_exit = function(code, signal) {
+        if (code === 20)
+            throw new Error("minisat reported that problem was [UNSATISFIABLE]");
+        if (code !== 10)
+            throw new Error("minisat exited with [" + code + "]");
+        // Parse and index cnf output.
+        console.log("parsing & indexing cnf");
+        var cnf_out = fs.readFileSync(pcnf_path, "utf-8");
+        var cnf_map = {};
+        cnf_out.split("\n").every(function(cnf_line) {
+            var cnf_match = cnf_line.match(/^c (\d+) (\w+)$/);
+            if (cnf_match === null)
+                return (cnf_line.match(/^p cnf/) === null);
+            var key = cnf_match[2];
+            var value = Number(cnf_match[1]);
+            cnf_map[key] = value;
+            return true;
+        });
+        // Parse and index solution.
+        console.log("parsing & indexing solution");
+        var cnf_out = fs.readFileSync(pout_path, "utf-8");
+        if (cnf_out.match(/^SAT\n/) === null)
+            throw new Error("did not find [SAT] marker in solution");
+        var raw_sol_v = cnf_out.substring(4).split(" ");
+        var solution_v = Array(raw_sol_v.length + 1);
+        for (var i = 0; i < raw_sol_v.length; i++)
+            solution_v[i + 1] = (Number(raw_sol_v[i]) >= 0);
+        // Print human readable summary.
+        for (var key in main_bv) {
+            var number_v = "", data_v = "", cur_nb = 0, cur_tb = 0, cur_n2v = 1, cur_t2v = 0x80;
+            var flush_fn = function() {
+                number_v = z2(cur_nb.toString(16)) + number_v;
+                data_v = data_v + z2(cur_tb.toString(16));
+                cur_nb = 0;
+                cur_tb = 0;
+                cur_n2v = 1;
+                cur_t2v = 0x80;
+            };
+            main_bv[key].forEach(function(bit) {
+                assert(bit instanceof Bit);
+                var is_set = solution_v[cnf_map[bit.toString()]];
+                if (is_set) {
+                    cur_nb = (cur_nb | cur_n2v);
+                    cur_tb = (cur_tb | cur_t2v);
+                }
+                cur_n2v = (cur_n2v << 1);
+                cur_t2v = (cur_t2v >> 1);
+                if (cur_n2v === 0x100)
+                    flush_fn();
+            });
+            if (cur_n2v !== 1)
+                flush_fn();
+            var summary = key + "/n: 0x" + number_v + "\n"
+            + key + "/d: [" + data_v + "]";
+            console.log(summary);
+            fs.appendFileSync(psum_path, summary);
+        }
+        process.exit(0);
+    };
+};
---
+++ hashsat.js
@@ -0,0 +1,286 @@
+require('./boolicius.js');
+
+var k_bv_cache = null;
+
+var sha256_k_bv = function(t) {
+    var raw_k = [
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b,
+        0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
+        0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7,
+        0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152,
+        0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+        0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc,
+        0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819,
+        0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08,
+        0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f,
+        0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    ];
+    if (k_bv_cache === null)
+        k_bv_cache = new Array(raw_k.length);
+    if (k_bv_cache[t] === undefined)
+        k_bv_cache[t] = define_bv(32, raw_k[t]);
+    return k_bv_cache[t];
+};
+
+var sha256_ch = function(x, y, z) {
+    return xor_bv(and_bv(x, y), and_bv(not_bv(x), z));
+};
+
+var sha256_maj = function(x, y, z) {
+    return xor_bv(xor_bv(and_bv(x, y), and_bv(x, z)), and_bv(y, z));
+};
+
+var sha256_bsigma0 = function(w) {
+    return xor_bv(xor_bv(rotr_bv(w, 2), rotr_bv(w, 13)), rotr_bv(w, 22));
+};
+
+var sha256_bsigma1 = function(w) {
+    return xor_bv(xor_bv(rotr_bv(w, 6), rotr_bv(w, 11)), rotr_bv(w, 25));
+};
+
+var sha256_ssigma0 = function(w) {
+    return xor_bv(xor_bv(rotr_bv(w, 7), rotr_bv(w, 18)), shr_bv(w, 3));
+};
+
+var sha256_ssigma1 = function(w) {
+    return xor_bv(xor_bv(rotr_bv(w, 17), rotr_bv(w, 19)), shr_bv(w, 10));
+};
+
+var _debug_satisfy;
+
+var sha256_process = function(in_state, message) {
+    assert(message.length === 512);
+    // Initialize the array W.
+    var w = new Array(64);
+    for (var t = 0; t < 16; t++) {
+        w[t] = message.slice(t * 32, t * 32 + 32).reverse();
+        //_debug_satisfy["w" + t] = w[t];
+    }
+
+    for (var t = 16; t < 64; t++) {
+        w[t] = uaddm_bv(
+            sha256_ssigma1(w[t - 2]),
+            w[t - 7],
+            sha256_ssigma0(w[t - 15]),
+            w[t - 16]
+        );
+        //_debug_satisfy["w" + t] = w[t];
+    }
+
+    // Run all sha256 iterations.
+    var a = in_state.a, b = in_state.b, c = in_state.c, d = in_state.d
+    , e = in_state.e, f = in_state.f, g = in_state.g, h = in_state.h;
+
+
+
+    for (var t = 0; t < 64; t++) {
+
+        /*
+        _debug_satisfy["a" + t] = a;
+        _debug_satisfy["b" + t] = b;
+        _debug_satisfy["c" + t] = c;
+        _debug_satisfy["d" + t] = d;
+        _debug_satisfy["e" + t] = e;
+        _debug_satisfy["f" + t] = f;
+        _debug_satisfy["g" + t] = g;
+        _debug_satisfy["h" + t] = h;
+
+        if (t === 32) {
+            satisfy(_debug_satisfy);
+            throw new Error("foo");
+        }*/
+
+        var temp1 = uaddm_bv(
+            h,
+            sha256_bsigma1(e),
+            sha256_ch(e, f, g),
+            sha256_k_bv(t),
+            w[t]
+        );
+        var temp2 = uaddm_bv(
+            sha256_bsigma0(a),
+            sha256_maj(a, b, c)
+        );
+        h = g;
+        g = f;
+        f = e;
+        e = uaddm_bv(d, temp1);
+        d = c;
+        c = b;
+        b = a;
+        a = uaddm_bv(temp1, temp2);
+    }
+
+    /*
+    _debug_satisfy["a"] = a;
+    _debug_satisfy["b"] = b;
+    _debug_satisfy["c"] = c;
+    _debug_satisfy["d"] = d;
+    _debug_satisfy["e"] = e;
+    _debug_satisfy["f"] = f;
+    _debug_satisfy["g"] = g;
+    _debug_satisfy["h"] = h;
+    satisfy(_debug_satisfy);
+    throw new Error("foo");*/
+
+
+    // Do final add and return the output state.
+    var out_state = {
+        a: uaddm_bv(in_state.a, a),
+        b: uaddm_bv(in_state.b, b),
+        c: uaddm_bv(in_state.c, c),
+        d: uaddm_bv(in_state.d, d),
+        e: uaddm_bv(in_state.e, e),
+        f: uaddm_bv(in_state.f, f),
+        g: uaddm_bv(in_state.g, g),
+        h: uaddm_bv(in_state.h, h)
+    };
+
+    /*_debug_satisfy["a"] = out_state.a;
+    _debug_satisfy["b"] = out_state.b;
+    _debug_satisfy["c"] = out_state.c;
+    _debug_satisfy["d"] = out_state.d;
+    _debug_satisfy["e"] = out_state.e;
+    _debug_satisfy["f"] = out_state.f;
+    _debug_satisfy["g"] = out_state.g;
+    _debug_satisfy["h"] = out_state.h;
+    satisfy(_debug_satisfy);
+    throw new Error("foo");*/
+
+    return out_state;
+};
+
+var sha256 = function(main_in) {
+    assert((main_in.length % 8) === 0);
+    var state = {
+        a: define_bv(32, 0x6A09E667),
+        b: define_bv(32, 0xBB67AE85),
+        c: define_bv(32, 0x3C6EF372),
+        d: define_bv(32, 0xA54FF53A),
+        e: define_bv(32, 0x510E527F),
+        f: define_bv(32, 0x9B05688C),
+        g: define_bv(32, 0x1F83D9AB),
+        h: define_bv(32, 0x5BE0CD19)
+    };
+    // Pad main_in according to the sha256 specification.
+    // Suppose a message has length L < 2^64.  Before it is input to the
+    // hash function, the message is padded on the right as follows:
+    //  a.  "1" is appended.
+    var p_main_in = main_in.slice(0);
+    p_main_in.push(define_bit(true));
+    //  b.  K "0"s are appended where K is the smallest, non-negative
+    //      solution to the equation L + 1 + K = 448 (mod 512)
+    while ((p_main_in.length % 512) !== 448)
+        p_main_in.push(define_bit(false));
+    //  c.  Then append the 64-bit block that is L in binary representation.
+    //      After appending this block, the length of the message will be a
+    //      multiple of 512 bits.
+    p_main_in = p_main_in.concat(define_bv(32, 0), define_bv(32, main_in.length).reverse());
+    assert((p_main_in.length % 512) === 0);
+    // Iterate over all blocks and produce a new state.
+    for (var i = 0; i < p_main_in.length; i += 512)
+        state = sha256_process(state, p_main_in.slice(i, i + 512));
+    // The final message digest is the big endian encoded
+    // 32b integers in the state concatenated together.
+    return [].concat(
+        state.a.reverse(),
+        state.b.reverse(),
+        state.c.reverse(),
+        state.d.reverse(),
+        state.e.reverse(),
+        state.f.reverse(),
+        state.g.reverse(),
+        state.h.reverse()
+    );
+};
+
+/*var foo = define_bv_hex("1234");
+console.log(foo);
+process.exit(0);*/
+
+var main_input = define_bv_hex("666f6f626172");
+
+_debug_satisfy = {main_input: main_input};
+
+var main_output = sha256(sha256(main_input));
+
+// Solve it.
+satisfy({main_input: main_input, main_output: main_output});
+
+
+/*
+try {
+var main_output = sha256(main_input);
+} catch (e) {
+
+}*/
+
+
+/*
+
+
+var nonce = define_input_bv(32);
+
+// Compile main in bit vector. Numbers are little endian encoded.
+var main_in = [].concat(define_bv_from_hex(
+    "01000000" + // Version
+    "81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000" + // hashPrevBlock
+    "e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122b" + // hashMerkleRoot
+    "c7f5d74d" + // Time
+    "f2b9441a" // Target/difficulcy
+)).concat(
+    nonce // Nonce
+);
+
+// Create a double sha256 problem.
+var main_out = sha256(sha256(main_in));
+
+process.exit(0);
+
+// Solve it.
+satisfy({nonce: nonce, main_out: main_out});
+
+
+
+*/
+
+/*
+// Test a shitty hash function.
+var input = define_input_bv(32);
+var c = define_bv(32, 0x6A09E667);
+
+/// out_bv = 0x6A09E667 + ((i + 0x6A09E667) <<< 7)
+var out_bv = uadd_bv(c, rotr_bv(uadd_bv(input, c)[0], 7))[0];
+
+// Constrain the output to 0x12345678.
+constrain_bv(out_bv, 0x12345678, 0xffffffff);
+
+// Get satisfaction.
+satisfy({input: input});*/
+
+// Init SHA-256 state.
+//var state;
+
+/*
+    fstr_t data = "01000000" "81cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000" "e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122b" \
+    "c7f5d74d" "f2b9441a" "42a14695";
+    fstr_t datab = fss(fstr_hexdecode(data));
+
+    uint8_t out[32];
+    fstr_t out_fs = FSTR_PACK(out);
+
+    sha2_context ctx;
+    sha2_starts(&ctx, 0);
+    sha2_update(&ctx, (void*) datab.str, datab.len);
+    sha2_finish(&ctx, out);
+
+    sha2_starts(&ctx, 0);
+    sha2_update(&ctx, (void*) out_fs.str, out_fs.len);
+    sha2_finish(&ctx, out);
+
+    DBG(fss(fstr_hexencode(out_fs)));
+    lwt_exit(0);
+ */
